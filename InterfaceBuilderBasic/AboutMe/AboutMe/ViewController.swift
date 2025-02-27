b33b324d03be7e3133a39eaca90282c03f21e868
Ian <kiprotichian254@gmail.com>
2025-02-27 10:10:59 +0300

    About Me Project

---
+++ InterfaceBuilderBasic/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Student3 on 21/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
